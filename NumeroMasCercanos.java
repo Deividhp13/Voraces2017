47fb070a428397befce270873d1e46d1af18dd94
Deividhp13 <davidherrero96@gmail.com>
2017-11-25 12:27:34 +0100

    Inicial

---
+++ NumeroMasCercanos.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+/**
+ * Created by matrix on 30/10/17.
+ */
+public class NumeroMasCercanos {
+
+
+    public NumeroMasCercanos() {
+
+    }
+
+    public long[] VorazNumerosCercanos(ArrayList<Integer> conjuntoNumeros){
+
+
+
+    /* Metodo que recibe como parametro un ArrayList de números entre 1 y 9
+       e implementa un algoritmo basado en un esquema Voraz para devolver
+       los dos numeros mas cercanos que se pueden generar a partir de todos
+       los numeros del ArrayList.
+       Se pueden utilizar otros métodos adicionales
+
+   */
+
+    }
+
+
+
+
+
+
+}
+
