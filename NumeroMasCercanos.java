8053f7bb8c9626099fd5114579de3574ede820de
Deividhp13 <davidherrero96@gmail.com>
2017-11-25 17:44:59 +0100

    asdasdasd

--- NumeroMasCercanos.java
+++ NumeroMasCercanos.java
@@ -14,9 +14,33 @@ public class NumeroMasCercanos {
     }
 
     public long[] VorazNumerosCercanos(ArrayList<Integer> conjuntoNumeros){
-       
+        UtilArrays utilidades = new UtilArrays();
+    ArrayList<Integer> solucionA = new ArrayList<>(conjuntoNumeros.size());
+    ArrayList<Integer> solucionB = new ArrayList<>(conjuntoNumeros.size());
+    int numeroA = 0, numeroB = 0, c;
+    while(!conjuntoNumeros.isEmpty()){
+        c = seleccionarCandidato(conjuntoNumeros);
+        conjuntoNumeros.remove(new Integer(c));
+                if(numeroA > numeroB){
+                    solucionB.add(c);
+                    numeroB += c;
+                } else{
+                    solucionA.add(c);
+                    numeroA += c;
+                }
+    }
+    utilidades.imprimeArray(solucionA);
+    utilidades.imprimeArray(solucionB);
+    return null;
     }
 
+    private int seleccionarCandidato(ArrayList<Integer> candidatos){
+    int c = candidatos.get(0);
+    for(int i = 1; i < candidatos.size(); i++)
+        if(c < candidatos.get(i)) c = candidatos.get(i);
+    return c;
+    }
+}
 
 
 
