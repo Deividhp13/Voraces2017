95f8ad9e99c59a338a70f85d795d6b03bc8eeaea
Deividhp13 <davidherrero96@gmail.com>
2017-11-26 18:35:54 +0100

    Falta obtener la optima

--- NumeroMasCercanos.java
+++ NumeroMasCercanos.java
@@ -19,17 +19,37 @@ public class NumeroMasCercanos {
         ArrayList<Integer> solucion2 = new ArrayList<>(conjuntoNumeros.size());
         ArrayList<Integer> sobrantes = new ArrayList<>(conjuntoNumeros.size());
         long[] sol = new long[2];
-        int candidato = 9, c = 0;
+        int candidato = 9, c;
         boolean fin = false;
         while (!fin) {
-            for (int i = 0; i < conjuntoNumeros.size(); i++) {
-                if (conjuntoNumeros.get(i) == candidato) solucion1.add(candidato);
+            c = 0;
+            for(int i= 0; i < conjuntoNumeros.size(); i++){
+                if(conjuntoNumeros.get(i) == candidato) c++;
+            }
+            if (c > 0){
+                if(c % 2 == 0){
+                    for(int i = 0; i < c; i++){
+                        if(i % 2 == 0) solucion1.add(candidato);
+                        else solucion2.add(candidato);
+                    }
+                }
+                else{
+                    sobrantes.add(candidato);
+                    c--;
+                    for(int i = 0; i < c; i++){
+                        if(i % 2 == 0) solucion1.add(candidato);
+                        else solucion2.add(candidato);
+                    }
+                }
             }
             candidato--;
             if(candidato == 0) fin = true;
         }
         utilidades.imprimeArray(conjuntoNumeros);
         utilidades.imprimeArray(solucion1);
+        utilidades.imprimeArray(solucion1);
+        Collections.sort(sobrantes);
+        utilidades.imprimeArray(sobrantes);
         return sol;
 
     }
