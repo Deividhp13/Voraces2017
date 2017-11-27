a51460d271f39e3dd157862eed23909aa4e16503
Deividhp13 <davidherrero96@gmail.com>
2017-11-27 22:32:16 +0100

    Hecho

--- NumeroMasCercanos.java
+++ NumeroMasCercanos.java
@@ -1,4 +1,3 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -12,27 +11,42 @@ public class NumeroMasCercanos {
     public NumeroMasCercanos() {
 
     }
-
+    /*
+    Función que recibe por parámetro un ArrayList de números enteros y devuelve un long[] con dos números
+    cuya diferencia es la menor en la mayoría de los casos. Por tanto, la función es óptima en una gran mayoría de
+    casos.
+     */
     public long[] VorazNumerosCercanos(ArrayList<Integer> conjuntoNumeros) {
         UtilArrays utilidades = new UtilArrays();
-        ArrayList<Integer> solucion1 = new ArrayList<>(conjuntoNumeros.size());
-        ArrayList<Integer> solucion2 = new ArrayList<>(conjuntoNumeros.size());
-        ArrayList<Integer> sobrantes = new ArrayList<>(conjuntoNumeros.size());
+        //Inicialización de las estructuras en las que tenemos que almacenar la solución.
+        //Ambos números se irán formando con los elementos de solucion1 y solucion2.
+        ArrayList<Integer> solucion1 = new ArrayList<>();
+        ArrayList<Integer> solucion2 = new ArrayList<>();
+        //En este ArrayList meteremos los elementos cuyo número es impar. Por ejemplo, si hay 3 treses, se almacena uno aquí.
+        ArrayList<Integer> sobrantes = new ArrayList<>();
+        //Inicialización del vector que devolverá
         long[] sol = new long[2];
+        //Candidato es el mayor número posible. Va reduciéndose. C es el contador de candidatos.
         int candidato = 9, c;
+        //fin indicará cuando se ha recorrido el conjunto de candidatos completamente
         boolean fin = false;
         while (!fin) {
+            //Inicializamos el contador a 0 al empezar la vuelta.
             c = 0;
+            //Se recorre el ArrayList para averiguar el número de candidatos que hay.
             for(int i= 0; i < conjuntoNumeros.size(); i++){
                 if(conjuntoNumeros.get(i) == candidato) c++;
             }
+            //Si hay más de 0 candidatos, entramos en este if (CANDIDATO ACEPTABLE)
             if (c > 0){
+                //Si el número de candidatos es par, asignamos a cada número la mitad de ellos.
                 if(c % 2 == 0){
                     for(int i = 0; i < c; i++){
                         if(i % 2 == 0) solucion1.add(candidato);
                         else solucion2.add(candidato);
                     }
                 }
+                //Si no es par, añadimos un candidato al ArrayList de sobrantes, y asignamos la mitad de los restantes a un número y la otra mitad al otro.
                 else{
                     sobrantes.add(candidato);
                     c--;
@@ -42,39 +56,57 @@ public class NumeroMasCercanos {
                     }
                 }
             }
+            //Se selecciona el siguiente candidato
             candidato--;
             if(candidato == 0) fin = true;
         }
-        Collections.sort(sobrantes);
-        int diferencia = Integer.MAX_VALUE;
-        int numero1 = 0, numero2 = 0;
-        int pos = 0;
-        for (int i=0; i < sobrantes.size()-1; i++) {
-            if (sobrantes.get(i + 1) - sobrantes.get(i) <= diferencia && numero1 < sobrantes.get(i + 1) && numero2 < sobrantes.get(i)) {
-                diferencia = sobrantes.get(i + 1) - sobrantes.get(i);
-                numero1 = sobrantes.get(i + 1);
-                numero2 = sobrantes.get(i);
-                pos = i;
+        /*
+        A partir de este punto, tenemos dos números exactamente iguales, cuya diferencia es 0. El objetivo es obtener una solución lo más óptima
+        posible. Para ello, realizamos el siguiente conjunto de pasos:
+         */
+        //Ordenamos el array de sobrantes de menor a mayor.
+        if(!sobrantes.isEmpty()) {
+            Collections.sort(sobrantes);
+            //Inicializamos la diferencia al máximo posible, y los dos números iniciales a 0, así como la posición.
+            int diferencia = Integer.MAX_VALUE;
+            int numero1 = 0, numero2 = 0;
+            int pos = 0;
+            //Obtenemos los dos números mayores que tengan la menor diferencia entre ellos, y añadimos uno al número 1 y el anterior al número 2.
+            for (int i = 0; i < sobrantes.size() - 1; i++) {
+                if (sobrantes.get(i + 1) - sobrantes.get(i) <= diferencia && numero1 < sobrantes.get(i + 1) && numero2 < sobrantes.get(i)) {
+                    diferencia = sobrantes.get(i + 1) - sobrantes.get(i);
+                    numero1 = sobrantes.get(i + 1);
+                    numero2 = sobrantes.get(i);
+                    pos = i;
+                }
             }
+            solucion1.add(numero1);
+            solucion2.add(numero2);
+            //Quitamos estos dos elementos del array de sobrantes
+            sobrantes.remove(pos);
+            sobrantes.remove(pos);
+            //Hallamos el número medio para recorrer el array de sobrantes hasta la mitad.
+            int mitad = (sobrantes.size() / 2);
+            //Metemos la primera mitad (los números menores del array) en el número que tenía el número más mayor, en orden de menor a mayor.
+            for (int i = 0; i < mitad; i++) {
+                solucion1.add(sobrantes.get(i));
+            }
+            //Revertimos el array de forma que queda de mayor a menor de nuevo.
+            Collections.reverse(sobrantes);
+            //Metemos los números de mayor a menor en el segundo número
+            for (int i = 0; i < mitad; i++) {
+                solucion2.add(sobrantes.get(i));
+            }
+            //Limpiamos el array de sobrantes, todos los elementos están colocados.
+            sobrantes.clear();
         }
-        solucion1.add(numero1);
-        solucion2.add(numero2);
-        sobrantes.remove(pos);
-        sobrantes.remove(pos);
-        int mitad = (sobrantes.size()/2);
-        for(int i = 0; i < mitad; i++){
-            solucion1.add(sobrantes.get(i));
-        }
-        Collections.reverse(sobrantes);
-        for(int i = 0; i < mitad; i++){
-            solucion2.add(sobrantes.get(i));
-        }
-        sobrantes.clear();
         utilidades.imprimeArray(conjuntoNumeros);
+        //Para meter los números en el vector, metemos los números de 1 en 1, multiplicando en cada paso por 10 para avanzar una posición.
         for(int i = 0; i < solucion1.size(); i++){
             sol[0] = sol[0] * 10 + solucion1.get(i);
             sol[1] = sol[1] * 10 + solucion2.get(i);
         }
+        //Devolvemos el vector.
         return sol;
 
     }
--- Pruebas.java
+++ Pruebas.java
@@ -1,20 +1,152 @@
 import java.util.ArrayList;
 
 public class Pruebas {
-    private static final int cifras=36;
+    /*
+    NOTA IMPORTANTE: Haciendo las pruebas, me he dado cuenta de que es imposible hacerlas con más de 36 cifras debido al
+    formato de long. long puede almacenar un entero con signo de 64-bits, desde -9,223,372,036,854,775,808 hasta 9,223,372,036,854,775,807.
+    Debido a ello, las pruebas que se realizan con 38 cifras (enteros de 19 cifras) y con 40 (enteros de 20) suelen dar error,
+    como se puede comprobar en el siguiente resultado:
+
+    Los 38 números del ArrayList son: 4,5,9,1,4,5,9,1,8,3,1,6,6,7,5,9,8,2,5,4,8,9,5,6,4,4,8,2,1,3,6,8,1,6,1,5,5,8
+    Los 19 números del ArrayList son: 9,9,8,8,8,6,6,5,5,5,4,4,3,2,1,1,1,7,4
+    Los 19 números del ArrayList son: 9,9,8,8,8,6,6,5,5,5,4,4,3,2,1,1,1,6,5
+    El primer  Numero es: -8457877518266340442
+    El segundo Numero es: -8457877518266340451
+    Los 40 números del ArrayList son: 9,5,8,8,9,6,6,9,9,4,9,1,4,3,8,4,3,7,8,2,3,5,4,4,4,4,1,2,1,7,4,9,2,6,6,3,5,7,4,8
+    Los 20 números del ArrayList son: 9,9,9,8,8,7,6,6,5,4,4,4,4,3,3,2,1,8,1,2
+    Los 20 números del ArrayList son: 9,9,9,8,8,7,6,6,5,4,4,4,4,3,3,2,1,7,5,4
+    El primer  Numero es: 7755046175895563732
+    El segundo Numero es: 7755046175895563674
+
+     */
+    private static final int cifra1=2;
+    private static final int cifra2=4;
+    private static final int cifra3=6;
+    private static final int cifra4=8;
+    private static final int cifra5=10;
+    private static final int cifra6=12;
+    private static final int cifra7=14;
+    private static final int cifra8=16;
+    private static final int cifra9=18;
+    private static final int cifra10=20;
+    private static final int cifra11=22;
+    private static final int cifra12=24;
+    private static final int cifra13=26;
+    private static final int cifra14=28;
+    private static final int cifra15=30;
+    private static final int cifra16=32;
+    private static final int cifra17=34;
+    private static final int cifra18=36;
+    //private static final int cifra19=38;
+    //private static final int cifra20=40;
+
+
     public static void main(String[] args){
         NumeroMasCercanos rm = new NumeroMasCercanos();
         UtilArrays genNumeros=new UtilArrays();
         ArrayList<Integer> conjuntoCifras;
         long [] resul;
 
-        conjuntoCifras=genNumeros.rellenaCandidatos(cifras);
+        conjuntoCifras=genNumeros.rellenaCandidatos(cifra1);
         resul=rm.VorazNumerosCercanos(conjuntoCifras);
-
         System.out.println("El primer  Numero es: " +resul[0]);
         System.out.println("El segundo Numero es: " +resul[1]);
 
+        conjuntoCifras=genNumeros.rellenaCandidatos(cifra2);
+        resul=rm.VorazNumerosCercanos(conjuntoCifras);
+        System.out.println("El primer  Numero es: " +resul[0]);
+        System.out.println("El segundo Numero es: " +resul[1]);
 
-        }
+        conjuntoCifras=genNumeros.rellenaCandidatos(cifra3);
+        resul=rm.VorazNumerosCercanos(conjuntoCifras);
+        System.out.println("El primer  Numero es: " +resul[0]);
+        System.out.println("El segundo Numero es: " +resul[1]);
+
+        conjuntoCifras=genNumeros.rellenaCandidatos(cifra4);
+        resul=rm.VorazNumerosCercanos(conjuntoCifras);
+        System.out.println("El primer  Numero es: " +resul[0]);
+        System.out.println("El segundo Numero es: " +resul[1]);
+
+        conjuntoCifras=genNumeros.rellenaCandidatos(cifra5);
+        resul=rm.VorazNumerosCercanos(conjuntoCifras);
+        System.out.println("El primer  Numero es: " +resul[0]);
+        System.out.println("El segundo Numero es: " +resul[1]);
+
+        conjuntoCifras=genNumeros.rellenaCandidatos(cifra6);
+        resul=rm.VorazNumerosCercanos(conjuntoCifras);
+        System.out.println("El primer  Numero es: " +resul[0]);
+        System.out.println("El segundo Numero es: " +resul[1]);
+
+        conjuntoCifras=genNumeros.rellenaCandidatos(cifra7);
+        resul=rm.VorazNumerosCercanos(conjuntoCifras);
+        System.out.println("El primer  Numero es: " +resul[0]);
+        System.out.println("El segundo Numero es: " +resul[1]);
+
+        conjuntoCifras=genNumeros.rellenaCandidatos(cifra8);
+        resul=rm.VorazNumerosCercanos(conjuntoCifras);
+        System.out.println("El primer  Numero es: " +resul[0]);
+        System.out.println("El segundo Numero es: " +resul[1]);
+
+        conjuntoCifras=genNumeros.rellenaCandidatos(cifra9);
+        resul=rm.VorazNumerosCercanos(conjuntoCifras);
+        System.out.println("El primer  Numero es: " +resul[0]);
+        System.out.println("El segundo Numero es: " +resul[1]);
+
+        conjuntoCifras=genNumeros.rellenaCandidatos(cifra10);
+        resul=rm.VorazNumerosCercanos(conjuntoCifras);
+        System.out.println("El primer  Numero es: " +resul[0]);
+        System.out.println("El segundo Numero es: " +resul[1]);
+
+        conjuntoCifras=genNumeros.rellenaCandidatos(cifra11);
+        resul=rm.VorazNumerosCercanos(conjuntoCifras);
+        System.out.println("El primer  Numero es: " +resul[0]);
+        System.out.println("El segundo Numero es: " +resul[1]);
+
+        conjuntoCifras=genNumeros.rellenaCandidatos(cifra12);
+        resul=rm.VorazNumerosCercanos(conjuntoCifras);
+        System.out.println("El primer  Numero es: " +resul[0]);
+        System.out.println("El segundo Numero es: " +resul[1]);
+
+        conjuntoCifras=genNumeros.rellenaCandidatos(cifra13);
+        resul=rm.VorazNumerosCercanos(conjuntoCifras);
+        System.out.println("El primer  Numero es: " +resul[0]);
+        System.out.println("El segundo Numero es: " +resul[1]);
+
+        conjuntoCifras=genNumeros.rellenaCandidatos(cifra14);
+        resul=rm.VorazNumerosCercanos(conjuntoCifras);
+        System.out.println("El primer  Numero es: " +resul[0]);
+        System.out.println("El segundo Numero es: " +resul[1]);
+
+        conjuntoCifras=genNumeros.rellenaCandidatos(cifra15);
+        resul=rm.VorazNumerosCercanos(conjuntoCifras);
+        System.out.println("El primer  Numero es: " +resul[0]);
+        System.out.println("El segundo Numero es: " +resul[1]);
+
+        conjuntoCifras=genNumeros.rellenaCandidatos(cifra16);
+        resul=rm.VorazNumerosCercanos(conjuntoCifras);
+        System.out.println("El primer  Numero es: " +resul[0]);
+        System.out.println("El segundo Numero es: " +resul[1]);
+
+        conjuntoCifras=genNumeros.rellenaCandidatos(cifra17);
+        resul=rm.VorazNumerosCercanos(conjuntoCifras);
+        System.out.println("El primer  Numero es: " +resul[0]);
+        System.out.println("El segundo Numero es: " +resul[1]);
+
+        conjuntoCifras=genNumeros.rellenaCandidatos(cifra18);
+        resul=rm.VorazNumerosCercanos(conjuntoCifras);
+        System.out.println("El primer  Numero es: " +resul[0]);
+        System.out.println("El segundo Numero es: " +resul[1]);
+        /*
+        conjuntoCifras=genNumeros.rellenaCandidatos(cifra19);
+        resul=rm.VorazNumerosCercanos(conjuntoCifras);
+        System.out.println("El primer  Numero es: " +resul[0]);
+        System.out.println("El segundo Numero es: " +resul[1]);
+
+        conjuntoCifras=genNumeros.rellenaCandidatos(cifra20);
+        resul=rm.VorazNumerosCercanos(conjuntoCifras);
+        System.out.println("El primer  Numero es: " +resul[0]);
+        System.out.println("El segundo Numero es: " +resul[1]);
+        */
+                }
     }
 
