5dd210f7b5fbb741074e9f1059b2bc2792a1e96b
Deividhp13 <davidherrero96@gmail.com>
2017-11-27 17:26:31 +0100

    No es óptima

--- NumeroMasCercanos.java
+++ NumeroMasCercanos.java
@@ -45,10 +45,40 @@ public class NumeroMasCercanos {
             candidato--;
             if(candidato == 0) fin = true;
         }
+        Collections.sort(sobrantes);
+        utilidades.imprimeArray(sobrantes);
+        int diferencia = Integer.MAX_VALUE;
+        int numero1 = 0, numero2 = 0;
+        int pos = 0;
+        for (int i=0; i < sobrantes.size()-1; i++) {
+            if (sobrantes.get(i + 1) - sobrantes.get(i) <= diferencia && numero1 < sobrantes.get(i + 1) && numero2 < sobrantes.get(i)) {
+                diferencia = sobrantes.get(i + 1) - sobrantes.get(i);
+                numero1 = sobrantes.get(i + 1);
+                numero2 = sobrantes.get(i);
+                pos = i;
+            }
+        }
+        solucion1.add(numero1);
+        solucion2.add(numero2);
+        sobrantes.remove(pos);
+        sobrantes.remove(pos);
+        System.out.println("*/TRAAAZAAAAAAAA*/");
+        int mitad = (sobrantes.size()/2);
+        System.out.println(mitad);
+        System.out.println("*/TRAAAZAAAAAAAA*/");
+        for(int i = 0; i < mitad; i++){
+            solucion1.add(sobrantes.get(i));
+        }
+        Collections.reverse(sobrantes);
+        for(int i = 0; i < mitad; i++){
+            solucion2.add(sobrantes.get(i));
+        }
+        sobrantes.clear();
         utilidades.imprimeArray(conjuntoNumeros);
         utilidades.imprimeArray(solucion1);
-        utilidades.imprimeArray(solucion1);
-        Collections.sort(sobrantes);
+        utilidades.imprimeArray(solucion2);
+
+
         utilidades.imprimeArray(sobrantes);
         return sol;
 
